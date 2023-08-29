c8539d0feb3ce4f63688ce86e8af9e70def73a73
Vitaliy Tkachenko <tkachenko.java@yandex.ru>
2023-08-29 13:12:37 +0500

    add axios instance

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = __IS_DEV__ ? 'http://localhost:8080' : 'http://back:8080'
+
 const API = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
