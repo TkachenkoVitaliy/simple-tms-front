03dbf8a4d5624d27d794cdb76022da5a239ce791
Vitaliy Tkachenko <tkachenko.java@yandex.ru>
2023-09-11 18:33:28 +0500

    add resizable

--- src/pages/TestsPage/ui/TestsPage.tsx
+++ src/pages/TestsPage/ui/TestsPage.tsx
@@ -1,13 +1,39 @@
+import { useTheme } from '@mui/material'
+import { Resizable } from 're-resizable'
 import { Outlet } from 'react-router-dom'
 
+const style = {
+  minHeight: '100%',
+  height: '100%',
+  padding: '16px',
+}
+
 function TestsPage() {
+  const theme = useTheme()
+  const { divider } = theme.palette
+  console.log(divider)
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
-      <div style={{ width: '100%', height: '100%' }}>TestsPage</div>
-      <div
-        className="testContent"
-        style={{ width: '100%', height: '100%' }}
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        overflow: 'hidden',
+        height: '100%',
+      }}
+    >
+      <Resizable
+        style={{ ...style, borderRight: `2px solid ${divider}` }}
+        defaultSize={{
+          width: '30%',
+          height: '100%',
+        }}
+        maxWidth="80%"
+        minWidth="10%"
       >
+        <div style={{ width: '100%', height: '100%' }}>TestsPage</div>
+      </Resizable>
+      <div style={{ ...style, width: '100%', minWidth: '1px' }}>
         <Outlet />
       </div>
     </div>
