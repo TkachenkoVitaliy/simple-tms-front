0d7007946a5a272b7c4512fd2a7c52e344337fad
Vitaliy Tkachenko <tkachenko.java@yandex.ru>
2023-09-22 13:28:26 +0500

    add StepEditor

--- src/entities/TestCase/ui/StepEditor/StepEditor.tsx
+++ src/entities/TestCase/ui/StepEditor/StepEditor.tsx
@@ -1,7 +1,50 @@
+import MDEditor from '@uiw/react-md-editor'
 import { memo } from 'react'
 
-export interface StepEditorProps {}
+export interface StepValue {
+  action: string
+  expected: string
+}
+
+export interface StepEditorProps {
+  value: StepValue
+  onChange: (value: StepValue, id?: number | string | undefined) => void
+  id?: number | string
+  index: number
+}
 
 export const StepEditor = memo((props: StepEditorProps) => {
-  return <div>Step</div>
+  const { value, onChange, id, index } = props
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div>{index + 1}</div>
+      <div>
+        Action
+        <MDEditor
+          style={{ minHeight: '200px' }}
+          height="100%"
+          visibleDragbar={false}
+          value={value.action}
+          onChange={(newVal) =>
+            onChange({ action: newVal || '', expected: value.expected }, id)
+          }
+          preview="edit"
+        />
+      </div>
+      <div>
+        Expected
+        <MDEditor
+          style={{ minHeight: '200px' }}
+          height="100%"
+          visibleDragbar={false}
+          value={value.expected}
+          onChange={(newVal) =>
+            onChange({ action: value.action, expected: newVal || '' }, id)
+          }
+          preview="edit"
+        />
+      </div>
+    </div>
+  )
 })
