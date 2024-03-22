663e35c61c8901fb40660a0499986493bb631f9e
Tkachenko Vitaliy <tkachenko.java@yandex.ru>
2024-03-22 09:18:12 +0500

    continue 2

--- src/entities/TestCase/ui/RepeatableStepSelector/RepeatableStepSelector.tsx
+++ src/entities/TestCase/ui/RepeatableStepSelector/RepeatableStepSelector.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
-import DraftsIcon from '@mui/icons-material/Drafts'
-import InboxIcon from '@mui/icons-material/Inbox'
-import { Dialog, DialogContent, List } from '@mui/material'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  IconButton,
+  List,
+  Typography,
+} from '@mui/material'
 
 import { TestStepRepeatable } from 'entities/TestCase/model/types/testCase'
+
 import { PageableList } from 'shared/ui/PageableList/PageableList'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+
+import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown'
+import ListItemText from '@mui/material/ListItemText'
+import ListItem from '@mui/material/ListItem'
 
 export interface RepeatableStepSelectorProps {
   open: boolean
@@ -43,6 +51,30 @@ export const RepeatableStepSelector = (props: RepeatableStepSelectorProps) => {
     // console.log(reason)
   }
 
+  const renderFunc = useCallback((item: TestStepRepeatable) => {
+    return (
+      <ListItem key={item.id}>
+        <IconButton
+          onClick={() => {
+            console.log(item.id)
+          }}
+        >
+          <ExpandCircleDownIcon />
+        </IconButton>
+        <Button
+          sx={{ textAlign: 'start', justifyContent: 'flex-start' }}
+          color="inherit"
+          fullWidth
+          onClick={() => {
+            console.log('ITEM ', item.id)
+          }}
+        >
+          <Typography>{item.name}</Typography>
+        </Button>
+      </ListItem>
+    )
+  }, [])
+
   return (
     <Dialog
       fullWidth
@@ -58,8 +90,7 @@ export const RepeatableStepSelector = (props: RepeatableStepSelectorProps) => {
           data={data}
           getId={(item) => item.id}
           getLabel={(item) => item.name}
-          itemsPerPage={10}
-          onSelect={(id) => console.log(id)}
+          itemRenderFunc={renderFunc}
         />
       </div>
     </Dialog>
--- src/shared/ui/PageableList/PageableList.tsx
+++ src/shared/ui/PageableList/PageableList.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+
 import { List } from '@mui/material'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
@@ -6,37 +8,34 @@ export interface PageableListProps<T, ID extends string | number> {
   data: T[]
   getLabel: (item: T) => string
   getId: (item: T) => ID
-  itemsPerPage: number
   onSelect?: (id: ID) => unknown
+  itemRenderFunc?: (item: T) => JSX.Element
 }
 
 export function PageableList<T, ID extends string | number>(
   props: PageableListProps<T, ID>,
 ) {
-  const { data, getLabel, getId, itemsPerPage, onSelect } = props
+  const { data, getLabel, getId, onSelect, itemRenderFunc } = props
 
-  const createItem = (
-    onSelect: PageableListProps<T, ID>['onSelect'],
-    getId: PageableListProps<T, ID>['getId'],
-    getLabel: PageableListProps<T, ID>['getLabel'],
-    item: T,
-  ) => {
-    if (onSelect === undefined) {
-      return <ListItem key={getId(item)}>{getLabel(item)}</ListItem>
-    }
-    return (
-      <ListItemButton
-        key={getId(item)}
-        onClick={() => onSelect(getId(item))}
-      >
-        {getLabel(item)}
-      </ListItemButton>
-    )
-  }
-
-  return (
-    <List>
-      {data.map((item) => createItem(onSelect, getId, getLabel, item))}
-    </List>
+  const createItem = useCallback(
+    (item: T) => {
+      if (itemRenderFunc !== undefined) {
+        return itemRenderFunc(item)
+      }
+      if (onSelect === undefined) {
+        return <ListItem key={getId(item)}>{getLabel(item)}</ListItem>
+      }
+      return (
+        <ListItemButton
+          key={getId(item)}
+          onClick={() => onSelect(getId(item))}
+        >
+          {getLabel(item)}
+        </ListItemButton>
+      )
+    },
+    [onSelect, getLabel, getId, itemRenderFunc],
   )
+
+  return <List>{data.map((item) => createItem(item))}</List>
 }
