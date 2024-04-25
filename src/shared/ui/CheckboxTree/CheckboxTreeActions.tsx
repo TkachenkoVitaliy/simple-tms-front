c2ef8c8f8867541d4e68d4c35d00d3e08f662188
Tkachenko Vitaliy <tkachenko.java@yandex.ru>
2024-04-25 08:59:07 +0500

    continue actions

--- src/shared/ui/CheckboxTree/CheckboxTreeActions.tsx
+++ src/shared/ui/CheckboxTree/CheckboxTreeActions.tsx
@@ -1,13 +1,14 @@
 import { Button } from '@mui/material'
 
 import { useCheckboxTreeContext } from 'shared/ui/CheckboxTree/useCheckboxTreeContext'
+import { useMemo } from 'react'
 
 export const CheckboxTreeActions = () => {
   const [treeCheckState, setTreeCheckState] = useCheckboxTreeContext()
 
   const changeExpandStateAll = (expand: boolean) => {
     const newState = new Map(treeCheckState)
-    newState.forEach((val, _) => {
+    newState.forEach((val) => {
       if (val.expanded !== undefined) {
         // eslint-disable-next-line no-param-reassign
         val.expanded = expand
@@ -16,10 +17,40 @@ export const CheckboxTreeActions = () => {
     setTreeCheckState(newState)
   }
 
+  const canExpandAll = useMemo(() => {
+    let collapsedCount = 0
+    treeCheckState.forEach((val) => {
+      if (val.expanded === false) {
+        collapsedCount += 1
+      }
+    })
+    return collapsedCount > 0
+  }, [treeCheckState])
+
+  const canCollapseAll = useMemo(() => {
+    let expandedCount = 0
+    treeCheckState.forEach((val) => {
+      if (val.expanded === true) {
+        expandedCount += 1
+      }
+    })
+    return expandedCount > 0
+  }, [treeCheckState])
+
   return (
     <div>
-      <Button onClick={() => changeExpandStateAll(true)}>EXPAND ALL</Button>
-      <Button onClick={() => changeExpandStateAll(false)}>COLLAPSE ALL</Button>
+      <Button
+        disabled={!canExpandAll}
+        onClick={() => changeExpandStateAll(true)}
+      >
+        EXPAND ALL
+      </Button>
+      <Button
+        disabled={!canCollapseAll}
+        onClick={() => changeExpandStateAll(false)}
+      >
+        COLLAPSE ALL
+      </Button>
     </div>
   )
 }
