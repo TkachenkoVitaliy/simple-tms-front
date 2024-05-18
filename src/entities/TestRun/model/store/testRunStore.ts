52a3f707d35d4d5a142f57707fb7b554d3b45a9a
Vitaliy Tkachenko <tkachenko.java@yandex.ru>
2024-05-18 12:10:18 +0500

    fix test run name

--- src/entities/TestRun/model/store/testRunStore.ts
+++ src/entities/TestRun/model/store/testRunStore.ts
@@ -33,9 +33,28 @@ export class TestRunStore {
   createTestRun = async (testPlanId: number, testPlanName: string) => {
     this.setLoading(true)
     const currentDate = new Date()
-    const name = `[${testPlanName}] - ${currentDate.getDate()}/${
-      currentDate.getMonth() + 1
-    }/${currentDate.getFullYear()} @ ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`
+    const date =
+      currentDate.getDate() < 10
+        ? `0${currentDate.getDate()}`
+        : currentDate.getDate().toString()
+    const month =
+      currentDate.getMonth() < 9
+        ? `0${currentDate.getMonth() + 1}`
+        : (currentDate.getMonth() + 1).toString()
+    const hours =
+      currentDate.getHours() < 10
+        ? `0${currentDate.getHours()}`
+        : currentDate.getHours().toString()
+    const minutes =
+      currentDate.getMinutes() < 10
+        ? `0${currentDate.getMinutes()}`
+        : currentDate.getMinutes().toString()
+    const seconds =
+      currentDate.getSeconds() < 10
+        ? `0${currentDate.getSeconds()}`
+        : currentDate.getSeconds().toString()
+    const name = `[${testPlanName}] - ${date}/${month}/${currentDate.getFullYear()} @ ${hours}:${minutes}:${seconds}`
+    console.log(name)
     const createTestRunRequest = { testPlanId, name }
     const response = await TestRunAPI.create(
       this.projectId,
